91e1d768263a9661576160f60033edefd603b1c4
Pavel <semerikovpn@gmail.com>
2024-02-16 04:08:37 +0300

    1.01c

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+# bot.py
 import discord
 from discord import Option
 from discord.ui import Button, View
@@ -13,37 +14,45 @@ from game_state import GameState
 
 bot = discord.Bot(intents=discord.Intents.default())
 
-game_state = GameState(bot, GAME_CHANNEL_ID)
-
 
 class RegisterButton(Button):
     def __init__(self, label: str, game_state: GameState):
         super().__init__(label=label, style=discord.ButtonStyle.green)
-        self.game_state = game_state  # Сохраняем ссылку на экземпляр GameState для использования в callback
+        self.game_state = game_state
 
     async def callback(self, interaction: discord.Interaction):
         user = interaction.user
-        is_full, response = await self.game_state.register_player(user)  # Попытка регистрации игрока
-        await interaction.response.send_message(response, ephemeral=True)  # Отправляем уведомление пользователю
+        is_full, response = await self.game_state.register_player(user, interaction)
+        await interaction.response.send_message(response, ephemeral=True)
+
+
+class VoteButton(Button):
+    def __init__(self, label: str, vote_type: str, game_state: GameState):
+        super().__init__(label=label, style=discord.ButtonStyle.green if vote_type == "agree" else discord.ButtonStyle.red)
+        self.vote_type = vote_type
+        self.game_state = game_state
+
+    async def callback(self, interaction: discord.Interaction):
+        await self.game_state.process_vote(interaction.user, self.vote_type)
+        await interaction.response.send_message(f"Ваш голос '{self.label}' учтен.", ephemeral=True)
+        self.game_state.voting_message = interaction.message
+
+
+game_state = GameState(bot, GAME_CHANNEL_ID)
 
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} подключился к Discord!')
-    #вывод сообщения в тестовый канал
     channel = bot.get_channel(GAME_CHANNEL_ID)
     if channel:
-        # Отправляем сообщение в канал о том, что бот подключился
         await channel.send("Бот подключен к Discord!")
 
 
 @bot.event
 async def on_slash_command_error(ctx, error):
-    if isinstance(error, commands.errors.CheckFailure):
-        await ctx.respond('У вас нет необходимых прав для использования этой команды.', ephemeral=True)
-    else:
-        await ctx.respond('При обработке вашей команды произошла ошибка.', ephemeral=True)
-        print(f'Ошибка в команде {ctx.command.name}: {error}')
+    await ctx.respond('При обработке вашей команды произошла ошибка.', ephemeral=True)
+    print(f'Ошибка в команде {ctx.command.name}: {error}')
 
 
 @bot.event
@@ -52,11 +61,9 @@ async def on_rate_limit(ctx, rate_limit_info):
     await ctx.respond('Бот сейчас ограничен в запросах. Пожалуйста, попробуйте позже.', ephemeral=True)
 
 
-@bot.slash_command(name='start_registration', description='Начать регистрацию игроков. Можно указать кол-во игроков на команду.')
+@bot.slash_command(name='start_registration', description='Начать регистрацию игроков.')
 async def start_registration(ctx, players_per_team: Option(int, "Введите количество игроков в команде", required=False, default=5)):
-    # Проверяем, что регистрация еще не началась
     if not await game_state.check_ready_to_start() and len(await game_state.get_registered_players()) == 0:
-        # Устанавливаем количество игроков в команде
         success, message = await game_state.set_players_per_team(players_per_team)
         if success:
             await ctx.respond(f"Регистрация началась. Количество игроков в команде установлено в {players_per_team}.", ephemeral=True)
@@ -65,20 +72,16 @@ async def start_registration(ctx, players_per_team: Option(int, "Введите 
     else:
         await ctx.respond("Регистрация уже началась или уже есть зарегистрированные игроки.", ephemeral=True)
 
-    # Создаем кнопку для регистрации с передачей game_state
     button = RegisterButton(label="Регистрация на матч", game_state=game_state)
     view = View()
     view.add_item(button)
-    await ctx.respond('Нажмите на кнопку для регистрации.', view=view)
+    await ctx.respond('Нажмите на кнопку для регистрации.', view=view, ephemeral=True)
 
 
 @bot.slash_command(name='register', description='Зарегистрироваться на текущий матч.')
-async def register(ctx):
-    is_full, response = await game_state.register_player(ctx.author)
-    await ctx.respond(response)
-    if is_full:
-        # Если регистрация полна, отобразите состав команд
-        await show_teams(ctx)
+async def register(interaction: discord.Interaction):
+    is_full, response = await game_state.register_player(interaction.user, interaction)
+    await interaction.response.send_message(response, ephemeral=True)
 
 
 @bot.slash_command(name='unregister', description='Отменить свою регистрацию на матч.')
@@ -89,104 +92,58 @@ async def unregister(ctx):
 
 @bot.slash_command(name='admin_register', description='Зарегистрировать игрока на матч командой администратора.', default_permission=False)
 @discord.default_permissions(administrator=True)
-async def admin_register(ctx, member: Option(discord.Member, "Выберите участника для регистрации")):
-    is_full, response = await game_state.register_player(member) 
-    await ctx.respond(response)
-    if is_full:
-        # Если регистрация полна, отобразите состав команд
-        await show_teams(ctx)
+async def admin_register(interaction: discord.Interaction, member: Option(discord.Member, "Выберите участника для регистрации")):
+    is_full, response = await game_state.register_player(member, interaction)
+    await interaction.response.send_message(response, ephemeral=True)
 
 
 @bot.slash_command(name='admin_unregister', description='Отменить регистрацию игрока командой администратора.', default_permission=False)
 @discord.default_permissions(administrator=True)
 async def admin_unregister(ctx, member: Option(discord.Member, "Выберите участника для отмены регистрации")):
-    response = await game_state.unregister_player(member) 
+    response = await game_state.unregister_player(member)
     await ctx.respond(response)
 
 
-@bot.slash_command(name='set_players', description='Изменить количество игроков в команде. Только для администраторов.', default_permission=False)
+@bot.slash_command(name='set_players', description='Изменить количество игроков в команде.', default_permission=False)
 @discord.default_permissions(administrator=True)
-async def set_players_per_team(ctx, number: Option(int, "Введите новое количество игроков в команде")):
-    is_full, response = await game_state.set_players_per_team(number)
-    if is_full:
-        # Если регистрация полна, отобразите состав команд
-        await show_teams(ctx)
-    else:
-        await ctx.respond(response)
+async def set_players_per_team(interaction: discord.Interaction, number: Option(int, "Введите новое количество игроков в команде")):
+    success, response = await game_state.set_players_per_team(number)
+    await interaction.response.send_message(response, ephemeral=True)
 
 
-@bot.slash_command(name='stop_registration', description='Остановить регистрацию и очистить список зарегистрированных игроков. Только для администраторов.', default_permission=False)
+@bot.slash_command(name='stop_registration', description='Остановить регистрацию и очистить список зарегистрированных игроков.', default_permission=False)
 @discord.default_permissions(administrator=True)
 async def stop_registration(ctx):
     response = await game_state.clear_registered_players()
-    activity = discord.Activity(type=discord.ActivityType.watching, name="на выдру")
-    await bot.change_presence(status=discord.Status.online, activity=activity)
-    await asyncio.sleep(3)
-    await bot.change_presence(status=discord.Status.online, activity=None)
     await ctx.respond(response)
 
 
-@bot.slash_command(name='reshuffle', description='Пересортировать команды.')
-@discord.default_permissions(administrator=True)
-async def reshuffle(ctx):
-    await game_state.reshuffle_teams()  # Пересортировка
-    # Отправка обновленного состава команд
-
-
-@bot.slash_command(name='show_teams', description='Показать текущий состав команд.')
-async def show_teams(ctx):
-    team1, team2 = await game_state.auto_split_teams()
-    # Форматирование и отправка сообщения с составом команд
-    if (await game_state.check_ready_to_start()):
-        await display_teams(ctx, team1, team2)
-    else:
-        await ctx.respond("Команды не сформированы", ephemeral=True)
-
-
-@bot.slash_command(name='info', description='Вывести информацию о зарегистрированных игроках.', default_permission=False)
+@bot.slash_command(name='info', description='Вывести информацию о зарегистрированных игроках.')
 async def info(ctx):
     players_per_team = await game_state.get_players_per_team()
     registered_players = await game_state.get_registered_players()
-    mid_index = len(registered_players) // 2
-        # Создаем Embed сообщение для более структурированного отображения информации
     embed_info = discord.Embed(
         title="**Информация о регистрации**",
-        color=0xFFA500  # Оранжевый цвет
+        color=0xFFA500
     )
     embed_info.add_field(
         name="Зарегистрировано игроков",
-        value=f"**{len(registered_players)} из {players_per_team*2}**",  
+        value=f"**{len(registered_players)} из {players_per_team*2}**",
         inline=False
     )
-    if registered_players:  # Проверяем, есть ли зарегистрированные игроки
-    # Формируем строку с упоминаниями всех зарегистрированных игроков
-        registered_players_mentions1 = "\n".join([f'- {player.mention}' for player in registered_players[mid_index:]])
-        registered_players_mentions2 = "\n".join([f'- {player.mention}' for player in registered_players[:mid_index]])
+    if registered_players:
+        registered_players_mentions = "\n".join([f'- {player.mention}' for player in registered_players])
         embed_info.add_field(
             name="Зарегистрированные игроки:",
-            value=registered_players_mentions1,
+            value=registered_players_mentions,
             inline=True
         )
-        embed_info.add_field(
-                name="📋",
-                value=registered_players_mentions2,
-                inline=True
-        )    
     else:
         embed_info.add_field(
             name="Зарегистрированные игроки",
             value="Игроки еще не зарегистрированы.",
             inline=True
         )
-    await ctx.respond(embed=embed_info)
-
-
-async def display_teams(ctx, team1, team2):
-    if await game_state.check_ready_to_start():  # Убедитесь, что используете await
-        embed_team1 = discord.Embed(title="**Команда 1**", description="\n".join([f'- {player.mention}' for player in team1]), color=0x0000FF)
-        embed_team2 = discord.Embed(title="**Команда 2**", description="\n".join([f'- {player.mention}' for player in team2]), color=0xFF0000)
-        await ctx.respond(embeds=[embed_team1, embed_team2])  # Используйте await для асинхронного вызова
-    else:
-        await ctx.respond("Команды не сформированы")
+    await ctx.respond(embed=embed_info, ephemeral=True)
 
 bot.run(TOKEN)
--- game_state.py
+++ game_state.py
@@ -1,3 +1,4 @@
+# game_state.py
 import random
 import discord
 import asyncio
@@ -9,15 +10,35 @@ class GameState:
         self.registered_players = []
         self.players_per_team = 5
         self.channel_id = channel_id
+        self.votes = {"agree": 0, "reshuffle": 0}
+        self.voting_active = False
+        self.voting_message = None  # Добавляем атрибут для хранения сообщения голосования
 
-    async def register_player(self, player):
+    async def process_vote(self, user, vote_type):
+        if not self.voting_active:
+            print("Голосование не активно.")
+            return
+
+        # Учет голосов
+        self.votes[vote_type] += 1
+        total_votes = sum(self.votes.values())
+
+        # Проверка условий окончания голосования
+        if total_votes >= len(self.registered_players):
+            await self.evaluate_votes()
+        else:
+            # Обновляем сообщение с текущим состоянием голосования (псевдокод)
+            if self.voting_message:
+                new_content = f"Текущее голосование: Согласны - {self.votes['agree']}, Перемешать - {self.votes['reshuffle']}"
+                await self.voting_message.edit(content=new_content)       
+
+    async def register_player(self, player, interaction):
         if player not in self.registered_players and len(self.registered_players) < self.players_per_team * 2:
             self.registered_players.append(player)
             await self.update_bot_status()
             if await self.check_ready_to_start():
-                team1, team2 = await self.auto_split_teams()
-                # Используйте self.channel_id здесь, без передачи как параметра
-                await self.display_teams(team1, team2)
+                # После регистрации последнего игрока отображаем команды с кнопками для голосования
+                await self.display_teams_with_voting(interaction)  # Предполагается, что этот метод уже включает логику отображения команд и добавления кнопок
                 return (True, 'Достигнуто максимальное количество игроков. Старт матча')
             return (False, f'{player.mention} зарегистрирован на матч. Игроков зарегистрировано {len(self.registered_players)} из {self.players_per_team * 2}.')
         return (False, f'{player.mention}, вы уже зарегистрированы или достигнуто максимальное количество игроков.')
@@ -68,8 +89,7 @@ class GameState:
         if await self.check_ready_to_start():
             activity = discord.Activity(type=discord.ActivityType.watching, name="матч")
             await self.bot.change_presence(status=discord.Status.online, activity=activity)
-            await asyncio.sleep(20)  # Задержка в 2 минуты (120 секунд)
-            await self.bot.change_presence(status=discord.Status.online, activity=None)
+            await asyncio.sleep(3)  
         elif self.registered_players:  
             activity = discord.Activity(type=discord.ActivityType.watching, name=f"{len(self.registered_players)}/{self.players_per_team*2} игроков")
             await self.bot.change_presence(status=discord.Status.online, activity=activity)
@@ -77,7 +97,7 @@ class GameState:
             activity = discord.Activity(type=discord.ActivityType.watching, name="Регистрация")
             await self.bot.change_presence(status=discord.Status.online, activity=activity)
 
-    async def display_teams(self, team1, team2):
+    async def display_teams(self, ctx, team1, team2):
         channel = self.bot.get_channel(self.channel_id)
         if channel:
             embed_team1 = discord.Embed(title="**Команда 1**", description="\n".join([f'- {player.mention}' for player in team1]), color=0x0000FF)
@@ -85,3 +105,66 @@ class GameState:
             await channel.send(embeds=[embed_team1, embed_team2])
         else:
             print("Невозможно отправить сообщение: канал не найден.")
+
+    async def evaluate_votes(self):
+        total_votes = sum(self.votes.values())
+        agree_percentage = self.votes["agree"] / total_votes
+        if agree_percentage >= VOTE_THRESHOLD:
+            await self.finalize_teams()
+        else:
+            await self.reshuffle_teams()
+        self.reset_votes()
+
+    async def reset_votes(self):
+        self.votes = {"agree": 0, "reshuffle": 0}
+        self.voting_active = False
+
+    async def finalize_teams(self):
+        # Предполагается, что team1 и team2 уже сформированы
+        team1, team2 = await self.auto_split_teams()
+
+        # Отправка сообщений в канал и участникам
+        channel = self.bot.get_channel(self.channel_id)
+        if channel:
+            await channel.send("Команды окончательно сформированы.")
+
+        for player in team1:
+            await player.send(f"Присоединяйтесь к голосовому каналу команды 1: {VOICE_CHANNEL_ID_TEAM1}")
+
+        for player in team2:
+            await player.send(f"Присоединяйтесь к голосовому каналу команды 2: {VOICE_CHANNEL_ID_TEAM2}")
+
+    async def display_teams_with_voting(self, interaction: discord.Interaction):
+        team1, team2 = await self.auto_split_teams()
+        if await self.check_ready_to_start():
+            self.voting_active = True
+
+    async def display_teams_with_voting(self, interaction: discord.Interaction):
+        team1, team2 = await self.auto_split_teams()
+        if await self.check_ready_to_start():
+            self.voting_active = True
+
+            # Check if the interaction has already been responded to
+            if not interaction.response.is_done():
+                # If not, defer the response as the next steps might take longer than the allowed time
+                await interaction.response.defer()
+
+            embed_team1 = discord.Embed(title="**Команда 1**", description="\n".join([f'- {player.mention}' for player in team1]), color=0x0000FF)
+            embed_team2 = discord.Embed(title="**Команда 2**", description="\n".join([f'- {player.mention}' for player in team2]), color=0xFF0000)
+
+            # Since we deferred, we now use followup.send
+            await interaction.followup.send("Команды сформированы:", embeds=[embed_team1, embed_team2], ephemeral=False)
+
+            # Добавление кнопок голосования
+            agree_button = VoteButton(label="Согласен", vote_type="agree", game_state=self)
+            reshuffle_button = VoteButton(label="Перемешать", vote_type="reshuffle", game_state=self)
+            view = View()
+            view.add_item(agree_button)
+            view.add_item(reshuffle_button)
+
+            # Отправка кнопок
+            await interaction.followup.send('Выберите действие:', view=view, ephemeral=False)
+
+    async def reshuffle_teams(self):
+        await self.shuffle_teams()
+        # Метод для пересортировки команд; дополнительная логика может быть добавлена здесь
